ab27f400a2e658cf4b95ab2354c759dafcfe7404
ekasai <ekasai.dev@gmail.com>
2025-12-02 13:47:03 +0900

    取引画面チャット用FRを作成

---
+++ src/app/Http/Requests/TransactionMessageRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionMessageRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'body'  => ['required', 'max:400'],
+            'image_path' => ['nullable', 'mimes:jpeg,png', 'max:5120'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'body.required' => '本文を入力してください',
+            'body.max'      => '本文は400文字以内で入力してください',
+            'image_path.mimes'   => '「.png」または「.jpeg」形式でアップロードしてください',
+            'image_path.max'      => '画像サイズは5MB以下にしてください。',
+        ];
+    }
+}
