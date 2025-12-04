d4c9b4892f7a0ddca8798894056d9cb329156f8b
ekasai <ekasai.dev@gmail.com>
2025-12-04 11:34:50 +0900

    不要な呼び出しを削除

--- src/app/Mail/TransactionCompletedMail.php
+++ src/app/Mail/TransactionCompletedMail.php
@@ -2,15 +2,10 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 
 class TransactionCompletedMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
     public $transaction;
 
     public function __construct($transaction)
